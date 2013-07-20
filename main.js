2b8ee983ba87a086ccb9b01cb12210d6884251ef
manymengofishing <mattheyj@googlemail.com>
2013-07-20 10:04:56 -0400

    added change functionality

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
-  (function(){
+(function(){
 
   // the minimum version of jQuery we want
-  var v = "1.3.2";
+  var v = "1.9.1";
 
   // check prior inclusion and version
   if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
@@ -25,48 +25,126 @@
     (window.myBookmarklet = function() {
 
       var $gridGuide = $("<div id='grid_guide'></div>)")
-      .css({'opacity':'1','position':'absolute','top':'0px','left':'0px','width':'100%','height':'100%','z-index':'9'});
-      $gridGuide.appendTo('body');
+      .css({'opacity':'1'
+        ,'position':'absolute'
+        ,'top':'0px'
+        ,'left':'0px'
+        ,'width':'100%'
+        ,'height':'100%'
+        ,'z-index':'9'});
 
-      var $btnShowGridGuide = $("<a href='#'></a>").
-      css({'position':"absolute",'text-align':'center','top':'0px','right':'0px','width':'30px','height':'30px','color':'white','background-color':'black','z-index':'99'})
+      var $form = $("#grid_guide_settings");
+      var $btnShowGridGuide = $("<a href='#'></a>")
+      .css({'position':"absolute"
+        ,'text-align':'center'
+        ,'top':'0px'
+        ,'right':'0px'
+        ,'width':'30px'
+        ,'height':'30px'
+        ,'color':'white'
+        ,'background-color':'black'
+        ,'z-index':'99'})
       .html('X');
-      $btnShowGridGuide.appendTo('body');
+
+      $gridGuide.appendTo('body');
+      $btnShowGridGuide.appendTo($gridGuide);
       $btnShowGridGuide.click(function(event){
         event.preventDefault();
         alert('show');
       });
 
-      var gridSettings = {
-        'hori':[["5","20px"],["5","100px"],["10","10px"]],
-        'vert':[["20","100px"]]
-      }
+      $form.change(function() {
+        console.log('Handler for .change() called.');
+        gridSettings = getGridSettings($form);
+        drawGridGuides(gridSettings);
+      });
 
+      gridSettings = getGridSettings($form);
       drawGridGuides(gridSettings);
 
     })();
 
+    function getGridSettings($form){
+      var gridSettings = {};
+      gridSettings.margins = [];
+      gridSettings.columns = [];
+      gridSettings.rows = [];
+
+      gridSettings.margins = {};
+      var $margins = $form.find('.margins ');
+      gridSettings.margins.top = $margins.children('.top').val()
+      gridSettings.margins.bottom = $margins.children('.bottom').val()
+      gridSettings.margins.left = $margins.children('.left').val()
+      gridSettings.margins.right = $margins.children('.right').val()
+
+      $form.find('.columns > .row').each(function(i,obj){
+        var arr = [];
+        arr.push($(this).children('[name="count"]').val());
+        arr.push($(this).children('[name="width"]').val());
+        arr.push($(this).children('[name="gutter"]').val());
+        gridSettings.columns.push(arr);
+      });
+
+      $form.find('.rows > .row').each(function(i,obj){
+        var arr = [];
+        arr.push($(this).children('[name="count"]').val());
+        arr.push($(this).children('[name="width"]').val());
+        arr.push($(this).children('[name="gutter"]').val());
+        gridSettings.rows.push(arr);
+      });
+      console.log(gridSettings);
+
+      return gridSettings;
+
+    }
+
     function drawGridGuides(gridSettings){
-      var $gridGuide = $('#grid_guide');
-      var topOffset = 0;
-      $.each(gridSettings.hori,function(i, obj){
-        var lineCount = obj[0];
-        var lineSpacingInt = parseInt(obj[1]);
-        for(var i = 0; i < lineCount; i++){
-          $("<div class='line'></div>").css({'height':'1px','background-color':'red','width':'100%','position':'absolute','left':'0px'})
-          .css('top',(topOffset + lineSpacingInt)+"px").appendTo($gridGuide);
-          topOffset += lineSpacingInt;
+      var $gridGuide = $('#grid_guide')
+      , leftOffset = parseInt(gridSettings.margins.left)
+      , topOffset = parseInt(gridSettings.margins.top);
+
+      $gridGuide.find('.guide-row').remove();
+      $gridGuide.find('.guide-col').remove();
+
+      $.each(gridSettings.rows,function(i, obj){
+        var count = obj[0];
+        var width = parseInt(obj[1]) - 2; // adjust for the two 1px line
+        var gutter = parseInt(obj[2]);
+        for(var i = 0; i < count; i++){
+          $("<div class='guide-row'></div>").css({
+            'height': width + 'px'
+            ,'border-top': '1px solid red'
+            ,'border-bottom': '1px solid red'
+            ,'width':'100%'
+            ,'position':'absolute'
+            ,'left':'0px'})
+          .css('top',(topOffset + width)+"px").appendTo($gridGuide);
+          topOffset += width + gutter;
         }
       });
 
-      var leftOffset = 0;
-      $.each(gridSettings.vert,function(i, obj){
-        var lineCount = obj[0];
-        var lineSpacingInt = parseInt(obj[1]);
-        for(var i = 0; i < lineCount; i++){
-          $("<div class='line'></div>").css({'height':'100%','background-color':'red','width':'1px','position':'absolute','top':'0px'})
-          .css('left',(leftOffset + lineSpacingInt)+"px").appendTo($gridGuide);
-          leftOffset += lineSpacingInt;
+      $.each(gridSettings.columns,function(i, obj){
+        var count = obj[0];
+        var gutter = parseInt(obj[2]);
+        var width = parseInt(obj[1]) - 2; // adjust for the two 1px line
+
+        if (gutter == 0){
+          width --;
+        }
+
+        for(var i = 0; i < count; i++){
+          var $col = $("<div class='guide-col'></div>").css({
+            'width': width + 'px'
+            ,'border-left': '1px solid red'
+            ,'height':'100%'
+            ,'position':'absolute'
+            ,'left':'0px'})
+          .css('left',(leftOffset + width)+"px");
+          if (gutter !== 0){
+            $col.css('border-right','1px solid red');
+          }
+          $col.appendTo($gridGuide);
+          leftOffset += width + gutter;
         }
       });
     }
